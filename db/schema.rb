92de2c0d2ab212466d76c9ed2762d0818d587b13
Michael Williams <michaelwilliamsdvc15@gmail.com>
2019-05-23 17:22:22 +0000

    troubleshoot

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_04_24_180856) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id"
   end
 
   create_table "places", force: :cascade do |t|
